efcdbc06663c25952e6f24cc5f98a8231d37b57e
unknown <rajeshkumar@gmail.com>
2025-09-01 09:36:16 +0530

    first commit

--- src/main/java/com/jaanavi/bookstore_microservices/BookstoreMicroservicesApplication.java
+++ src/main/java/com/jaanavi/bookstore_microservices/BookstoreMicroservicesApplication.java
@@ -1,12 +1,12 @@
-package com.jaanavi.bookstore.catalog;
+package com.jaanavi.bookstore_microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatalogServiceApplication {
+public class BookstoreMicroservicesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CatalogServiceApplication.class, args);
+        SpringApplication.run(BookstoreMicroservicesApplication.class, args);
     }
 }
---
+++ src/test/java/com/jaanavi/bookstore_microservices/BookstoreMicroservicesApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jaanavi.bookstore_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
